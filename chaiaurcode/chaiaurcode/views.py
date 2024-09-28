8fd8cc6d2290f2e2677d17c0d69a9326374421fb
Aryan Jain <aryanofficialjain@gmail.com>
2024-09-28 07:34:18 +0530

    django_learn

---
+++ chaiaurcode/chaiaurcode/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hello World ")
+    return render(request, 'website/index.html')
+
+
+def about(request):
+    return render(request, 'website/about.html')
+
+
+def contact(request):
+    return render(request, 'website/contact.html')
+
+
